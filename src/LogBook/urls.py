dc4a330634ae7a136faa2786aa6ab0ec77388fd6
heliomaster <luigidalmaso@gmail.com>
2016-12-04 17:49:03 +0100

    modif views

--- src/LogBook/urls.py
+++ src/LogBook/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.views.generic import *
 from . import views
 
 
-
-
 urlpatterns = [
+    # url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<sumup_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^$', views.index.as_view(), name='home'),
 ]
--- src/LogBook/views.py
+++ src/LogBook/views.py
@@ -1,45 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-<<<<<<< HEAD
-from django.http import HttpResponseRedirect
-from django.template import loader
-
-from .forms import NameForm
-
-
-# Create your views here.
-def get_name(request):
-    template = loader.get_template('name.html')
-    if request.method == 'POST':
-        form = NameForm(request.POST)
-        if form.is_valid():
-            return HttpResponseRedirect('/THANKS/')
-
-    else:
-        form = NameForm()
-    return render(request, 'name.html', {'form':form})
-=======
-#from .models import Logfields
-from .forms import LogfieldsForm
+from .models import Logfields
 
 
 def index(request):
-    titre = "title"
-    # log_fields = Logfields.objects.all()
-    # template = loader.get_template("LogBook/index.html")
-    context = {"template_title": titre, }
-    return render(request,"LogBook/index.html",context)
-    #add form
-    form = LogfieldsForm()
-    context = {
-        "title":title,
-        "form": form
-    }
-    return render(request, "index.html", context)
-
-
-
-
-def detail(request,sumup_id):
-    return HttpResponse("<h1>2eme page.</h1>")
->>>>>>> airportcode
+    posts = Logfields.object.pic_field()
+    return render(request, "index.html", {'posts': posts})
